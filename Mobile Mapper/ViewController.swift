71949468fd0c5bd3a6061891872008559f290ed3
Ashwin Kasargode <akasargode@Ashwins-MacBook-Air-3.local>
2019-03-08 11:50:33 -0600

    part 4

--- Mobile Mapper/ViewController.swift
+++ Mobile Mapper/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     let barringtonAnnotation = MKPointAnnotation()
+    let locationManager = CLLocationManager()
+    let address = "Springfield"
+    let geocoder = CLGeocoder()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let latitude = 42.15704
@@ -20,6 +25,17 @@ class ViewController: UIViewController {
         barringtonAnnotation.coordinate = coordinate
         barringtonAnnotation.title = "Barrington High School"
         mapView.addAnnotation(barringtonAnnotation)
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        geocoder.geocodeAddressString(address) {
+            (placemarks, error) in
+            for placemark in placemarks! {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = (placemark.location?.coordinate)!
+                annotation.title = placemark.name
+                self.mapView.addAnnotation(annotation)
+            }
+        }
     }
     
     
